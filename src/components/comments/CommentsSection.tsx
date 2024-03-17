b610b3d831e414f5301a599ca48100ad3ba00bdb
nour <nourwael1530@gmail.com>
2024-03-18 01:36:37 +0200

    change toSorted to sort due to vercel error

--- src/components/comments/CommentsSection.tsx
+++ src/components/comments/CommentsSection.tsx
@@ -64,16 +64,13 @@ const CommentsSection = async ({ postId }: ICommentsSection) => {
 
                 {/* render replies */}
                 {topLevelComment.replies
-                  .toSorted((a, b) => b.votes.length - a.votes.length)
+                  .sort((a, b) => b.votes.length - a.votes.length)
                   .map((reply) => {
-                    const replyVotesAmt = reply.votes.reduce(
-                      (acc, vote) => {
-                        if (vote.type === "UP") return acc + 1;
-                        if (vote.type === "DOWN") return acc - 1;
-                        return acc;
-                      },
-                      0
-                    );
+                    const replyVotesAmt = reply.votes.reduce((acc, vote) => {
+                      if (vote.type === "UP") return acc + 1;
+                      if (vote.type === "DOWN") return acc - 1;
+                      return acc;
+                    }, 0);
 
                     const replyVote = reply.votes.find(
                       (vote) => vote.userId === session?.user.id
